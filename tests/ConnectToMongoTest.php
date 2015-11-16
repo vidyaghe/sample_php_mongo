b11d0bc0de316141656101090f52da13ec46e075
vidyatestghe <vidyaghe@gmail.com>
2015-11-16 12:53:21 +0530

    Update ConnectToMongoTest.php

--- tests/ConnectToMongoTest.php
+++ tests/ConnectToMongoTest.php
@@ -7,7 +7,7 @@ class ConnectToMongoTest extends PHPUnit_Framework_TestCase{
       $collection = $connection->connect();
       $connection->insertDocument($collection, 'Seattle', 'WA');
       $result = $connection->findADocument($collection, 'Seattle');
-      $this->assertEquals('Seattle, WA', $result); //check if MongoDB returned 'Seattle, WA' 
+      $this->assertEquals('Seattle, aWA', $result); //check if MongoDB returned 'Seattle, WA' 
   }
 }
 ?>
